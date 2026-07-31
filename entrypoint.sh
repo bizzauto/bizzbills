5f2efe125240ab84b1111cb0dce8666e7bc5695d
BizzAuto <bizzauto@example.com>
2026-07-31 21:14:28 +0530

    fix: Remove prisma db push from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 set -e
 
-echo "⏳ Running Prisma migrations..."
-node ./node_modules/prisma/build/index.js db push --accept-data-loss
-
 echo "🚀 Starting application..."
 exec node server.js
